dc333d0c54e22e417eba9567b7f23fe4750c4d9b
Matt Amos <zerebubuth@gmail.com>
2013-04-30 00:34:25 +0100

    Refactor relation tests.

--- test/unit/relation_test.rb
+++ test/unit/relation_test.rb
@@ -9,86 +9,39 @@ class RelationTest < ActiveSupport::TestCase
   
   def test_from_xml_no_id
     noid = "<osm><relation version='12' changeset='23' /></osm>"
-    assert_nothing_raised(OSM::APIBadXMLError) {
-      Relation.from_xml(noid, true)
-    }
-    message = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(noid, false)
-    }
-    assert_match /ID is required when updating/, message.message
+    check_error_attr_new_ok(noid, Mime::XML, /ID is required when updating/)
   end
   
   def test_from_xml_no_changeset_id
     nocs = "<osm><relation id='123' version='12' /></osm>"
-    message_create = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(nocs, true)
-    }
-    assert_match /Changeset id is missing/, message_create.message
-    message_update = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(nocs, false)
-    }
-    assert_match /Changeset id is missing/, message_update.message
+    check_error_attr(nocs, Mime::XML, /Changeset id is missing/)
   end
   
   def test_from_xml_no_version
     no_version = "<osm><relation id='123' changeset='23' /></osm>"
-    assert_nothing_raised(OSM::APIBadXMLError) {
-      Relation.from_xml(no_version, true)
-    }
-    message_update = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(no_version, false)
-    }
-    assert_match /Version is required when updating/, message_update.message
+    check_error_attr_new_ok(no_version, Mime::XML, /Version is required when updating/)
   end
   
   def test_from_xml_id_zero
     id_list = ["", "0", "00", "0.0", "a"]
     id_list.each do |id|
       zero_id = "<osm><relation id='#{id}' changeset='332' version='23' /></osm>"
-      assert_nothing_raised(OSM::APIBadUserInput) {
-        Relation.from_xml(zero_id, true)
-      }
-      message_update = assert_raise(OSM::APIBadUserInput) {
-        Relation.from_xml(zero_id, false)
-      }
-      assert_match /ID of relation cannot be zero when updating/, message_update.message
+      check_error_attr_new_ok(zero_id, Mime::XML, /ID of relation cannot be zero when updating/, OSM::APIBadUserInput)
     end
   end
   
   def test_from_xml_no_text
-    no_text = ""
-    message_create = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(no_text, true)
-    }
-    assert_match /Must specify a string with one or more characters/, message_create.message
-    message_update = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(no_text, false)
-    }
-    assert_match /Must specify a string with one or more characters/, message_update.message
+    check_error_attr("", Mime::XML, /Must specify a string with one or more characters/)
   end
   
   def test_from_xml_no_k_v
     nokv = "<osm><relation id='23' changeset='23' version='23'><tag /></relation></osm>"
-    message_create = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(nokv, true)
-    }
-    assert_match /tag is missing key/, message_create.message
-    message_update = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(nokv, false)
-    }
-    assert_match /tag is missing key/, message_update.message
+    check_error_attr(nokv, Mime::XML, /tag is missing key/)
   end
   
   def test_from_xml_no_v
     no_v = "<osm><relation id='23' changeset='23' version='23'><tag k='key' /></relation></osm>"
-    message_create = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(no_v, true)
-    }
-    assert_match /tag is missing value/, message_create.message
-    message_update = assert_raise(OSM::APIBadXMLError) {
-      Relation.from_xml(no_v, false)
-    }
-    assert_match /tag is missing value/, message_update.message
+    check_error_attr(no_v, Mime::XML, /tag is missing value/)
   end
   
   def test_from_xml_duplicate_k
@@ -101,5 +54,32 @@ class RelationTest < ActiveSupport::TestCase
       Relation.from_xml(dupk, false)
     }
     assert_equal "Element relation/23 has duplicate tags with key dup", message_update.message
+  end
+
+  #### utility methods ####
+
+  # most attributes report faults in the same way, so we can abstract
+  # that to a utility method
+  def check_error_attr(content, format, message_regex)
+    message_create = assert_raise(OSM::APIBadXMLError) {
+      Relation.from_format(format, content, true)
+    }
+    assert_match message_regex, message_create.message
+    message_update = assert_raise(OSM::APIBadXMLError) {
+      Relation.from_format(format, content, false)
+    }
+    assert_match message_regex, message_update.message
+  end
+
+  # some attributes are optional on newly-created elements, but required
+  # on updating elements.
+  def check_error_attr_new_ok(content, format, message_regex, exception_class=OSM::APIBadXMLError)
+    assert_nothing_raised(exception_class) {
+      Relation.from_format(format, content, true)
+    }
+    message_update = assert_raise(exception_class) {
+      Relation.from_format(format, content, false)
+    }
+    assert_match message_regex, message_update.message
   end
 end
